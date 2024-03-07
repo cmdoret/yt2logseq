dc397cf706d90a914aa26e90e2d695887ae7cd9b
cmdoret <cyril.mattheydoret@gmail.com>
2024-03-07 03:25:11 +0100

    feat: pysrt to load subtitles

--- yt2logseq/srt.py
+++ yt2logseq/srt.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from langchain_core.documents import Document
 from langchain_community.document_loaders.base import BaseLoader
 import pysrt  # noqa:F401
@@ -7,11 +7,13 @@ import pysrt  # noqa:F401
 class StampedSRTLoader(BaseLoader):
     """Load `.srt` (subtitle) files."""
 
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, data: Optional[str]=None):
         """Initialize with a file path."""
         self.file_path = file_path
+        self.data = data
 
-    def load(self, chunk_size: int = 512) -> List[Document]:
+
+    def load(self, max_chunk_size: int = 512) -> List[Document]:
         """Load subtitles by chunks.
         Each chunk contains at most `chunk_size` tokens.
         Chunks always end at the end of a subtitle.
@@ -21,28 +23,35 @@ class StampedSRTLoader(BaseLoader):
         chunk_size
             The desired number of tokens in each chunks.
         """
-        parsed_info = pysrt.open(self.file_path)
-        current_chunk = ""
-        chunk_start = parsed_info[0].start.to_time()
+        if self.data:
+            srt = pysrt.from_string(self.data)
+        else:
+            srt = pysrt.open(self.file_path)
+        chunk_text = ""
+        chunk_size = 0
+        chunk_start = srt[0].start.to_time()
         chunks  = []
-        for line in parsed_info:
-            if len(current_chunk) + len(line.text) > chunk_size:
+        for line in srt:
+            line_size = len(line.text.split(" "))
+            if (chunk_size + line_size) > max_chunk_size:
                 metadata = {
                     "source": self.file_path,
                     'start': chunk_start,
                     'end': line.end.to_time()
                 }
-                chunks += [Document(page_content=current_chunk, metadata=metadata)]
-                current_chunk = ""
+                chunks += [Document(page_content=chunk_text, metadata=metadata)]
+                chunk_text = ""
+                chunk_size = 0
                 chunk_start = line.start.to_time()
-            current_chunk += line.text + " "
+            chunk_text += str(line)
+            chunk_size += line_size
         try:
             metadata = {
                 'source': self.file_path,
                 'start': chunk_start,
                 'end': line.end.to_time()
             }
-            chunks += [Document(page_content=current_chunk, metadata=metadata)]
+            chunks += [Document(page_content=chunk_text, metadata=metadata)]
             return chunks
         except UnboundLocalError:
             return []
