07eb553cc87a3881aee4b3cc36c459a25c817e56
cmdoret <cmdoret@mailbox.org>
2024-03-08 01:48:12 +0100

    feat: logseq timestamp formatting

--- yt2logseq/main.py
+++ yt2logseq/main.py
@@ -19,38 +19,20 @@ from typing import Iterator, Optional
 from pytube import YouTube
 from transformers import pipeline
 import whisper
+from whisper.utils import get_writer
 
-from yt2logseq.chains import make_mapreduce_chain
-from yt2logseq.srt import StampedSRTLoader
+from yt2logseq.chains import summarize_subtitles
+from yt2logseq.logseq import gather_logseq_topics, generate_logseq_page
 
-LOGSEQ_DIR=os.environ.get("LOGSEQ_DIR")
+LOGSEQ_DIR = os.environ.get("LOGSEQ_DIR")
 MAX_KEYWORDS=3
 MAX_SUMMARY_WORDS = 200
 
 
-def parse_topics_line(line: str) -> Iterator[str]:
-    """Parse a property line with topics in a line."""
-    # functionally equivalent to ripgrep -f "topic::" $LOGSEQ_DIR
-    values = line.split("::")[1].strip()
-    for value in values.split(","):
-        yield value.strip().strip("[]")
-
-
-def gather_logseq_topics(logseq_dir: Path) -> set[str]:
-    """Gather all topics from existing logseq notes."""
-    topics = set()
-    for file in Path(logseq_dir).rglob("*.md"):
-        with open(file, "r") as f:
-            for line in f:
-                if "topic::" in line:
-                    line_topics = parse_topics_line(line)
-                    topics |= set(line_topics)
-
-    return topics
 
 
 
-def download_audio(url: str) -> tuple[Path, YouTube]:
+def download_audio(url: str) -> tuple[Path, dict]:
     """Extract audio from target video and return output path+metadata."""
 
     yt = YouTube(url)
@@ -62,15 +44,33 @@ def download_audio(url: str) -> tuple[Path, YouTube]:
         .download(output_path=tempfile.gettempdir())
     )
 
-    return (Path(audio_path), yt)
+    meta = {
+        "title": yt.title,
+        "author": yt.author,
+        "description": yt.description.split("\n")[0],
+        "tags": yt.keywords,
+        "date": yt.publish_date.date() if yt.publish_date else None,
+        "url": yt.watch_url,
+    }
+
+    return (Path(audio_path), meta)
 
 
 def extract_subtitles(audio_file: Path) -> tuple[str, str]:
     """Extract subtitles and language from an audio file."""
 
     model = whisper.load_model("base")
-    result = model.transcribe(str(audio_file))
-    return (str(result["text"]), result['language'])
+    result = model.transcribe(
+        str(audio_file),
+    )
+    writer = get_writer(output_format='srt', output_dir=".")
+    writer(
+        result,
+        audio_file,
+        {"max_line_width":55, "max_line_count":2, "highlight_words":False}
+    )
+    subtitles = open(audio_file.with_suffix(".srt").name, 'r').read()
+    return (subtitles, result['language'])
 
 
 def assign_topics(
@@ -96,59 +96,20 @@ def assign_topics(
     ]
 
 
-def generate_logseq_page(summary: str, yt: YouTube, topics: Optional[list[str]]=None):
-    if not topics:
-        topics_line = ""
-    else:
-        topics_line = "topic:: " + ', '.join([f"[[{t}]]" for t in topics]) 
-    note = """
-type:: [[video]]
-title:: {title}
-author:: [[{author}]]
-date-published:: [[{date}]]
-description:: {description}
-tag:: {tags}
-language:: {language}
-link:: {url}
-{topics}
-
-- # Summary
-        - {summary}
-
-- # Video
-        - {{{{video {url}}}}}
-
-
-    """.format(
-        summary=summary,
-        topics=topics_line,
-        title=yt.title,
-        description=yt.description.split('\n')[0],
-        author=yt.author,
-        tags=', '.join(yt.keywords),
-        language=yt.language,
-        date=yt.publish_date.date(),
-        url=yt.watch_url,
-    )
-    return note 
 
 
 if __name__ == '__main__':
     url = sys.argv[1]
     output_file = sys.argv[2]
 
-    audio_path, yt = download_audio(url)
+    audio_path, meta = download_audio(url)
     subtitles, language = extract_subtitles(audio_path)
+    meta['language'] = language
     if LOGSEQ_DIR:
         all_topics = gather_logseq_topics(LOGSEQ_DIR)
-        topics = assign_topics(subtitles, tuple(all_topics))
-    else:
-        topics = None
-    docs = StampedSRTLoader(file_path="", data=subtitles).load()
-    mapreduce_chain = make_mapreduce_chain()
-    summary = mapreduce_chain.invoke({'input_documents': docs})
-    yt.language = language
-    page = generate_logseq_page(summary, yt, topics)
+        meta["topics"] = assign_topics(subtitles, tuple(all_topics))
+    summary = summarize_subtitles(subtitles)
+    page = generate_logseq_page(summary, meta)
     with open(output_file, "w") as out:
         out.write(page)
 
