b8e48ef652c163e236e5648ff6980dfa2fea0238
cmdoret <cmdoret@mailbox.org>
2024-06-08 00:26:34 +0200

    emphasis on keeping timestamps

--- yt2logseq/prompts.py
+++ yt2logseq/prompts.py
@@ -1,10 +1,10 @@
 
 MAP_TMPL = """The following are time-stamped captions in srt format.
 {docs}
-Based on these captions, please select and summarize the important information as bullet points in markdown format, making sure to include the original timestamp from the original caption where the information comes from..
+Based on these captions, please select and summarize the important information as bullet points in markdown format, making sure to always include the original timestamp from the original caption where the information comes from.
 Markdown summary:"""
 
 REDUCE_TMPL = """The following is set of summaries based on a video:
 {docs}
-Take these and distill it into a final, consolidated summary. Discard information that is not relevant. Retain the original time-stamps.
+Distill these into a final, consolidated summary. Discard information that is not relevant. Always retain the original time-stamps.
 Markdown summary:"""
